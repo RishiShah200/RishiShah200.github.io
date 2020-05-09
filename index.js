40366b677d32ba48deef73522c2282966fdcb2e0
Rishi Shah <rishishah200@gmail.com>
2020-05-08 21:19:19 -0400

    Changes

--- index.js
+++ index.js
@@ -27,3 +27,46 @@ document.addEventListener('DOMContentLoaded', () => {
 particlesJS.load('particles-js', 'particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
+
+
+
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+function getDocHeight() {
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    )
+}
+
+// var pctScrolled;
+
+// function amountscrolled() {
+//     var winheight = window.innerHeight || (document.documentElement || document.body).clientHeight
+//     var docheight = getDocHeight()
+//     var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+//     var trackLength = docheight - winheight
+//     pctScrolled = Math.floor(scrollTop / trackLength * 100) // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
+//     console.log(pctScrolled + '% scrolled')
+// }
+
+// window.addEventListener("scroll", function() {
+//     amountscrolled()
+//     if (pctScrolled > 40) {
+//         console.log("IT IS WORKING");
+//         const element = document.querySelector('.firstCard');
+//         element.classList.add('animate__animated', 'animate__bounceOutLeft');
+//     }
+// }, false)
+
+// const element = document.querySelector('#firstCard');
+const element = document.getElementById('firstCard');
+element.classList.add('animate__animated', 'animate__bounceOutLeft');
