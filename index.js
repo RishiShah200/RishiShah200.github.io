5406c6c5615bf9be115662eb51b0410a8873884b
Rishi Shah <rishishah200@gmail.com>
2020-05-09 16:02:42 -0400

    Added animations

--- index.js
+++ index.js
@@ -28,45 +28,82 @@ particlesJS.load('particles-js', 'particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
 
+var element_position = $('#cardRobotics').offset().top;
+var screen_height = $(window).height();
+var activation_offset = 1.0; //determines how far up the the page the element needs to be before triggering the function
+var activation_point = element_position - (screen_height * activation_offset);
+var max_scroll_height = $('body').height() - screen_height - 2; //-5 for a little bit of buffer
 
+//Does something when user scrolls to it OR
+//Does it when user has reached the bottom of the page and hasn't triggered the function yet
+$(window).on('scroll', function() {
+    var y_scroll_pos = window.pageYOffset;
 
-$.fn.isInViewport = function() {
-    var elementTop = $(this).offset().top;
-    var elementBottom = elementTop + $(this).outerHeight();
-    var viewportTop = $(window).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
-    return elementBottom > viewportTop && elementTop < viewportBottom;
-};
+    var element_in_view = y_scroll_pos > activation_point;
+    var has_reached_bottom_of_page = max_scroll_height <= y_scroll_pos && !element_in_view;
 
-function getDocHeight() {
-    var D = document;
-    return Math.max(
-        D.body.scrollHeight, D.documentElement.scrollHeight,
-        D.body.offsetHeight, D.documentElement.offsetHeight,
-        D.body.clientHeight, D.documentElement.clientHeight
-    )
-}
+    if (element_in_view || has_reached_bottom_of_page) {
+        const cardRobotics = document.querySelector('#cardRobotics');
+        cardRobotics.classList.add('animate__animated', 'animate__zoomIn');
+    }
+});
 
-// var pctScrolled;
+var element_position2 = $('#cardLaunchX').offset().top;
+var screen_height2 = $(window).height();
+var activation_offset2 = 1.0; //determines how far up the the page the element needs to be before triggering the function
+var activation_point2 = element_position2 - (screen_height2 * activation_offset2);
+var max_scroll_height2 = $('body').height() - screen_height2 - 5; //-5 for a little bit of buffer
 
-// function amountscrolled() {
-//     var winheight = window.innerHeight || (document.documentElement || document.body).clientHeight
-//     var docheight = getDocHeight()
-//     var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
-//     var trackLength = docheight - winheight
-//     pctScrolled = Math.floor(scrollTop / trackLength * 100) // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
-//     console.log(pctScrolled + '% scrolled')
-// }
+//Does something when user scrolls to it OR
+//Does it when user has reached the bottom of the page and hasn't triggered the function yet
+$(window).on('scroll', function() {
+    var y_scroll_pos2 = window.pageYOffset;
 
-// window.addEventListener("scroll", function() {
-//     amountscrolled()
-//     if (pctScrolled > 40) {
-//         console.log("IT IS WORKING");
-//         const element = document.querySelector('.firstCard');
-//         element.classList.add('animate__animated', 'animate__bounceOutLeft');
-//     }
-// }, false)
+    var element_in_view2 = y_scroll_pos2 > activation_point2;
+    var has_reached_bottom_of_page2 = max_scroll_height2 <= y_scroll_pos2 && !element_in_view2;
 
-// const element = document.querySelector('#firstCard');
-const element = document.getElementById('firstCard');
-element.classList.add('animate__animated', 'animate__bounceOutLeft');
+    if (element_in_view2 || has_reached_bottom_of_page2) {
+        const cardLaunchX = document.querySelector('#cardLaunchX');
+        cardLaunchX.classList.add('animate__animated', 'animate__zoomIn');
+    }
+});
+
+var element_position3 = $('#cardCSC').offset().top;
+var screen_height3 = $(window).height();
+var activation_offset3 = 1.0; //determines how far up the the page the element needs to be before triggering the function
+var activation_point3 = element_position3 - (screen_height3 * activation_offset3);
+var max_scroll_height3 = $('body').height() - screen_height3 - 5; //-5 for a little bit of buffer
+
+//Does something when user scrolls to it OR
+//Does it when user has reached the bottom of the page and hasn't triggered the function yet
+$(window).on('scroll', function() {
+    var y_scroll_pos3 = window.pageYOffset;
+
+    var element_in_view3 = y_scroll_pos3 > activation_point3;
+    var has_reached_bottom_of_page3 = max_scroll_height3 <= y_scroll_pos3 && !element_in_view3;
+
+    if (element_in_view3 || has_reached_bottom_of_page3) {
+        const cardCSC = document.querySelector('#cardCSC');
+        cardCSC.classList.add('animate__animated', 'animate__zoomIn');
+    }
+});
+
+var element_position4 = $('#cardChemOly').offset().top;
+var screen_height4 = $(window).height();
+var activation_offset4 = 1.0; //determines how far up the the page the element needs to be before triggering the function
+var activation_point4 = element_position4 - (screen_height4 * activation_offset4);
+var max_scroll_height4 = $('body').height() - screen_height4 - 5; //-5 for a little bit of buffer
+
+//Does something when user scrolls to it OR
+//Does it when user has reached the bottom of the page and hasn't triggered the function yet
+$(window).on('scroll', function() {
+    var y_scroll_pos4 = window.pageYOffset;
+
+    var element_in_view4 = y_scroll_pos4 > activation_point4;
+    var has_reached_bottom_of_page4 = max_scroll_height4 <= y_scroll_pos4 && !element_in_view4;
+
+    if (element_in_view4 || has_reached_bottom_of_page4) {
+        const cardChemOly = document.querySelector('#cardChemOly');
+        cardChemOly.classList.add('animate__animated', 'animate__zoomIn');
+    }
+});
